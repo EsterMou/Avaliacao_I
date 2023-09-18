de546123e30924377b464797cd311c88cdfa4d1a
Ester <55720120+EsterMou@users.noreply.github.com>
2023-09-18 18:44:46 -0300

    Update radarimpl.java

--- radarimpl.java
+++ radarimpl.java
@@ -1,5 +1,20 @@
+import java.util.Random;
+import java.util.RandomAccess;
+import java.util.List;
+
 import radar;
 
 public class radarimpl implements radar<velocidade> {
-    
+    private static final int VELOCIDADE_MEDIA = 60;
+    private static final int OSCILACAO_MAX = 100;
+
+    @Override
+    public List<velocidade> gerarLeituras(int totalLeituras){
+        List<velocidade> leituras = new ArrayList<>();
+
+        Random randomizador = new Random();
+        
+
+    }
+
 }
